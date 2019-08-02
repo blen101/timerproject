e93536a423317ce6b2479f6852e67f2785df5c84
Blen Yirga <blen@Blens-MacBook-Pro.local>
2019-08-02 18:45:14 +0300

    changes happened

--- lib/main.dart
+++ lib/main.dart
@@ -34,46 +34,6 @@ class _MyAppState extends State<MyApp> {
     super.dispose();
   }
 }
-class Timer extends StatelessWidget {
-  static const TextStyle timerTextStyle = TextStyle(
-    fontSize: 60,
-    fontWeight: FontWeight.bold,
-  );
-
-  @override
-  Widget build(BuildContext context) {
-    final TimerBloc _timerBloc = BlocProvider.of<TimerBloc>(context);
-    return Scaffold(
-      appBar: AppBar(title: Text('Flutter Timer')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 100.0),
-            child: Center(
-              child: BlocBuilder(
-                bloc: _timerBloc,
-                builder: (context, state) {
-                  final String minutesStr = ((state.duration / 60) % 60)
-                      .floor()
-                      .toString()
-                      .padLeft(2, '0');
-                  final String secondsStr =
-                      (state.duration % 60).floor().toString().padLeft(2, '0');
-                  return Text(
-                    '$minutesStr:$secondsStr',
-                    style: Timer.timerTextStyle,
-                  );
-                },
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
 class Actions extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -130,5 +90,51 @@ class Actions extends StatelessWidget {
       ];
     }
     return [];
+  }
+}
+class Timer extends StatelessWidget {
+  static const TextStyle timerTextStyle = TextStyle(
+    fontSize: 60,
+    fontWeight: FontWeight.bold,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final TimerBloc _timerBloc = BlocProvider.of<TimerBloc>(context);
+    return Scaffold(
+      appBar: AppBar(title: Text('Flutter Timer')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 100.0),
+            child: Center(
+              child: BlocBuilder(
+                bloc: _timerBloc,
+                builder: (context, state) {
+                  final String minutesStr = ((state.duration / 60) % 60)
+                      .floor()
+                      .toString()
+                      .padLeft(2, '0');
+                  final String secondsStr =
+                      (state.duration % 60).floor().toString().padLeft(2, '0');
+                  return Text(
+                    '$minutesStr:$secondsStr',
+                    style: Timer.timerTextStyle,
+                  );
+                },
+              ),
+            ),
+          ),
+          BlocBuilder(
+            condition: (previousState, currentState) =>
+                currentState.runtimeType != previousState.runtimeType,
+            bloc: _timerBloc,
+            builder: (context, state) => Actions(),
+          ),
+        ],
+      ),
+    );
   }
 }
